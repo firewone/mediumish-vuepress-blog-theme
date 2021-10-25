3fd87a87f5def4daa393a485d6152429916556a2
firewone <73457150+firewone@users.noreply.github.com>
2021-10-25 18:13:26 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-
+# a
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
