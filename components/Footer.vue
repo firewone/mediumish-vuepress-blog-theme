23c416bd30a5f83396ebeba6761ba7170f66cbd4
firewone <73457150+firewone@users.noreply.github.com>
2022-02-08 10:42:33 +0700

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -18,6 +18,16 @@ class="contact-item"
 {{ item.text }}
 </NavLink>
 </li>
+<li 
+v-for="item in contact" 
+:key="item.iconComponent" 
+class="contact-item">
+>
+<Navlink :link="item.link">
+<component :is="item.iconComponent"></component>
+{{ item.text }}
+</Navlink>
+</li>
 </ul>
 <ul v-if="copyright" class="list-unstyled">
 <li v-for="item in copyright" :key="item.text" class="copyright-item">
